9f31267205ec3d2636534f6074e369e9eab1181c
MoodyEG <moodyeg2011@yahoo.com>
2024-09-26 07:57:50 +0300

    Adding redis.js

---
+++ utils/redis.js
@@ -0,0 +1,58 @@
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.error(err);
+    });
+  }
+
+  isAlive() {
+    try {
+      this.client.ping();
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
