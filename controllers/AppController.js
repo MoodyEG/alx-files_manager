382fca2d2ae0ccc2865257230bc71786521152ef
MoodyEG <moodyeg2011@yahoo.com>
2024-09-27 06:57:54 +0300

    Adding First API

---
+++ controllers/AppController.js
@@ -0,0 +1,24 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+export default class AppController {
+  static getHomepage(req, res) {
+    res.status(200).send('Hello my friend!');
+  }
+
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static async getStats(req, res) {
+    const [users, files] = await Promise.all([
+      dbClient.nbUsers(),
+      dbClient.nbFiles(),
+    ]);
+
+    res.status(200).json({ users, files });
+  }
+}
---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import router from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/', router);
+app.use('/status', router);
+app.use('/stats', router);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+export default app;
