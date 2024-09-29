97e6ce0ab80af6d0b3b6ac351c5bc8e4413c85f0
MoodyEG <moodyeg2011@yahoo.com>
2024-09-29 11:46:22 +0300

    Testing file data Fixes 0.3

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -189,13 +189,13 @@ export default class FilesController {
       }
     }
     if (file.type === 'folder') {
-      return res.status(400).send({ error: 'A folder doesn t have content' });
+      return res.status(400).send({ error: 'A folder doesn\'t have content' });
     }
     const fileExist = await fs.access(file.localPath).then(() => true).catch(() => false);
     if (!file.localPath || !fileExist) {
       return res.status(404).send({ error: 'Not found' });
     }
-    const type = mime.lookup(file.name);
+    const type = mime.contentType(file.name);
     if (!type) {
       return res.status(404).send({ error: 'Not found' });
     }
