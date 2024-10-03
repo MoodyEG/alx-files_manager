dbb42c164af68930bfdae4f19614e82e55a40927
ahmedalaeldin <alaa.mido515@yahoo.com>
2024-10-02 18:04:51 -0700

    New Update Task 4

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-// Task 4 Main
+// Task 4 Main Code//
 export default class AuthController {
   static async getConnect(req, res) {
     try {
